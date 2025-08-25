35173e85c5fb1a29d13857700dd36202044b85b4
Leandro Vidal <leandro.vidal@securitize.io>
2025-08-24 21:36:43 -0300

    GitHub Actions first deployment - fix bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket  = "devops-practice-lmv"
+        bucket  = "devops-practice-lmv-dev"
         key     = "dp_001/terraform/tfstate/infra.tfstate"
         region  = "us-east-1"
         encrypt = true
