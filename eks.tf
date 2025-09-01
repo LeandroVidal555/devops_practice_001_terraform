0761ec2b296ff3842bd9de70d636fc0d333d8727
Leandro Vidal <leandro.vidal@securitize.io>
2025-09-01 12:39:45 -0300

    enable managed node group

--- eks.tf
+++ eks.tf
@@ -23,23 +23,23 @@ module "eks" {
   # Let the creator be cluster admin
   enable_cluster_creator_admin_permissions = true
 
-  #eks_managed_node_groups = {
-  #  workers = {
-  #    kubernetes_version = local.eks_cluster.k8s_version
-  #    capacity_type      = local.eks_cluster.capacity_type
-  #    instance_types     = local.eks_cluster.instance_types
+  eks_managed_node_groups = {
+    workers = {
+      kubernetes_version = local.eks_cluster.k8s_version
+      capacity_type      = local.eks_cluster.capacity_type
+      instance_types     = local.eks_cluster.instance_types
 
-  #    min_size     = local.eks_cluster.min_size
-  #    desired_size = local.eks_cluster.desired_size
-  #    max_size     = local.eks_cluster.max_size
+      min_size     = local.eks_cluster.min_size
+      desired_size = local.eks_cluster.desired_size
+      max_size     = local.eks_cluster.max_size
 
-  #    ami_type  = local.eks_cluster.ami_type
-  #    disk_size = local.eks_cluster.disk_size
+      ami_type  = local.eks_cluster.ami_type
+      disk_size = local.eks_cluster.disk_size
 
-  #    subnet_ids = module.vpc.private_subnets
-  #    labels     = { workload = "general_workers" }
-  #  }
-  #}
+      subnet_ids = module.vpc.private_subnets
+      labels     = { workload = "general_workers" }
+    }
+  }
 }
 
 resource "aws_eks_access_entry" "admin_roles" {
