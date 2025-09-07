f707fd4ca5d4d644d4ac6f84aa859651dc73eb1e
Leandro Vidal <leandro.vidal@securitize.io>
2025-09-07 00:22:42 -0300

    m4g instance + require no inputs for destroy

--- dev/eks.tf
+++ dev/eks.tf
@@ -8,7 +8,7 @@ module "eks" {
   endpoint_private_access = local.eks_cluster.endpoint_private_access
 
   endpoint_public_access       = var.enable_public_api ? true : false
-  endpoint_public_access_cidrs = var.enable_public_api && var.github_actions_egress_cidr != null ? [var.github_actions_egress_cidr] : ["127.0.0.1/32"] # ignored when public access is false
+  endpoint_public_access_cidrs = var.enable_public_api && var.github_actions_egress_cidr != null ? [var.github_actions_egress_cidr] : [] # ignored when public access is false
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets # control plane ENIs + nodes in private subnets
