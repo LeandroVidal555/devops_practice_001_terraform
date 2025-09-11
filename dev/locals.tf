a0408c744f42659058d8df1ee83ceaebae87af21
Leandro Vidal <leandro.vidal@securitize.io>
2025-09-10 23:29:02 -0300

    back to t3a workers for compatibility

--- dev/locals.tf
+++ dev/locals.tf
@@ -9,11 +9,11 @@ locals {
     endpoint_private_access = true
     cluster_version         = "1.33"
     capacity_type           = "SPOT"
-    instance_types          = ["t4g.medium"]
+    instance_types          = ["t3a.medium"]
     desired_size            = 1
     min_size                = 0
     max_size                = 2
-    ami_type                = "AL2023_ARM_64_STANDARD"
+    ami_type                = "AL2023_x86_64_STANDARD"
     disk_size               = 20
     admin_roles = [
       "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AdministratorAccess_${var.admin_sso_role_hash}",
