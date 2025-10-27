1d7a05e58f51fd8dff4aaf9f4788691dc7a70fb1
Leandro Vidal <leandro.vidal@securitize.io>
2025-10-27 12:38:39 -0300

    deploy metrics server after managed node group

--- dev/metrics_server_hpa.tf
+++ dev/metrics_server_hpa.tf
@@ -1,4 +1,9 @@
 resource "helm_release" "metrics_server" {
+  depends_on = [
+    module.mng_workers,
+    aws_eks_addon.coredns
+  ]
+
   name       = "${var.env}-${var.common_prefix}-metrics-server"
   namespace  = "kube-system"
   repository = "https://kubernetes-sigs.github.io/metrics-server/"
