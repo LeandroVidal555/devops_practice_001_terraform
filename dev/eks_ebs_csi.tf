e2d1ad26611f8966accd23f91daa22a4f1ca6b3a
Leandro Vidal <leandro.vidal@securitize.io>
2025-10-31 16:51:53 -0300

    add EBS CSI addon for dynamic storage creating/binding

---
+++ dev/eks_ebs_csi.tf
@@ -0,0 +1,53 @@
+# IAM role for the EBS CSI controller SA
+resource "aws_iam_role" "ebs_csi_controller" {
+  name = "${module.eks.cluster_name}-ebs-csi-controller"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Federated = data.aws_iam_openid_connect_provider.eks.arn
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          "${module.eks.oidc_provider}:aud" = "sts.amazonaws.com"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_policy" {
+  role       = aws_iam_role.ebs_csi_controller.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+# Addon for dynamic storage creating/binding
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name             = module.eks.cluster_name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_csi_controller.arn
+}
+
+resource "kubernetes_storage_class_v1" "gp3_default" {
+  depends_on = [aws_eks_addon.ebs_csi]
+
+  storage_provisioner    = "ebs.csi.aws.com"
+  reclaim_policy         = "Delete"
+  volume_binding_mode    = "WaitForFirstConsumer"
+  allow_volume_expansion = true
+
+  parameters = {
+    type = "gp3"
+  }
+
+  metadata {
+    name = "gp3"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+}
